3eb81b58b1c44b13f046558f0b0667117a490980
Pradeepkumar Gayam <in3xes@gmail.com>
2011-06-12 05:44:06 +0530

    Work in progress

--- master/buildbot/steps/source/mercurial.py
+++ master/buildbot/steps/source/mercurial.py
@@ -18,15 +18,12 @@ from twisted.internet import defer
 
 from buildbot.process.buildstep import LoggedRemoteCommand, RemoteShellCommand
 from buildbot.steps.source import Source
-from buildbot.status.results import SUCCESS, WARNINGS, FAILURE, SKIPPED, \
-     EXCEPTION, RETRY, worst_status
-
 
 class Mercurial(Source):
     """ Class for Mercurial with all the smarts """
     name = "hg"
 
-    def __init__(self, repourl=None, baseURL=None, mode='incremental',defaultBranch=None,
+    def __init__(self, repourl=None, baseurl=None, mode='incremental',defaultBranch=None,
                  branchType='inrepo', clobberOnBranchChange=True, **kwargs):
 
         """
@@ -37,23 +34,23 @@ class Mercurial(Source):
                         'inrepo'.  It is an error to specify a branch without
                         setting the C{branchType} to 'inrepo'.
 
-        @param baseURL: if 'dirname' branches are enabled, this is the base URL
+        @param baseurl: if 'dirname' branches are enabled, this is the base URL
                         to which a branch name will be appended. It should
                         probably end in a slash.  Use exactly one of C{repourl}
-                        and C{baseURL}.
+                        and C{baseurl}.
 
         @param defaultBranch: if branches are enabled, this is the branch
                               to use if the Build does not specify one
                               explicitly.
                               For 'dirname' branches, It will simply be
-                              appended to C{baseURL} and the result handed to
+                              appended to C{baseurl} and the result handed to
                               the 'hg update' command.
                               For 'inrepo' branches, this specifies the named
                               revision to which the tree will update after a
                               clone.
 
         @param branchType: either 'dirname' or 'inrepo' depending on whether
-                           the branch name should be appended to the C{baseURL}
+                           the branch name should be appended to the C{baseurl}
                            or the branch is a mercurial named branch and can be
                            found within the C{repourl}
 
@@ -64,22 +61,22 @@ class Mercurial(Source):
         """
         
         self.repourl = repourl
-        self.baseURL = baseURL
+        self.baseurl = baseurl
         self.branch = defaultBranch
         self.branchType = branchType
         self.clobberOnBranchChange = clobberOnBranchChange
         Source.__init__(self, **kwargs)
         self.mode = mode
         self.addFactoryArguments(repourl=repourl,
-                                 baseURL=baseURL,
+                                 baseurl=baseurl,
                                  mode=mode,
                                  defaultBranch=defaultBranch,
                                  branchType=branchType,
                                  )
 
-        if repourl and baseURL:
+        if repourl and baseurl:
             raise ValueError("you must provide exactly one of repourl and"
-                             " baseURL")
+                             " baseurl")
 
     def startVC(self, branch, revision, patch):
 
@@ -88,49 +85,61 @@ class Mercurial(Source):
             raise BuildSlaveTooOldError("slave is too old, does not know "
                                         "about hg")
 
-        if self.repourl:
-            assert self.branchType == 'inrepo'
-            self.repourl = self.computeRepositoryURL(self.repourl)
-        else:
-            self.repourl = self.computeRepositoryURL(self.baseURL) + (self.branch or '')
-
         if branch:
+            assert self.branchType == 'dirname' and not self.repourl
+            # The restriction is we can't configure named branch here.
+            # that's why 'not self.repourl'.
+            self.repourl = self.computeRepositoryURL(self.baseurl) + (branch or '')
             self.branch = branch
-
-        assert self.mode in ['incremental', 'clobber', 'fresh']
+        else:
+            assert self.branchType == 'inrepo' and not self.baseurl
+            self.repourl = self.computeRepositoryURL(self.repourl)
+            
+        assert self.mode in ['incremental', 'clobber', 'fresh', 'clean']
         self.stdio_log = self.addLog("stdio")
 
         d = defer.succeed(None)
         if self.mode == 'incremental':
             d.addCallback(self.incremental)
-        if self.mode == 'clobber':
+        elif self.mode == 'clobber':
             d.addCallback(self.doClobber)
+        elif self.mode == 'fresh':
+            d.addCallback(self.fresh)
+        elif self.mode == 'clean':
+            d.addCallback(self.clean)
+
         d.addCallback(self._parseGotRevision)
         return d
 
     def _dovccmd(self, command, end=True):
         self.cmd = cmd = RemoteShellCommand(self.workdir, ['hg', '--verbose'] + command)
-        self.cmd.useLog(self.stdio_log, end)
+        self.cmd.useLog(self.stdio_log, False)
         log.msg("Mercurial command : %s" % ("hg ".join(command), ))
         d = self.runCommand(self.cmd)
+        # remove this 'end' parameter.
+        # This 'end' was entirely bad idea.
         if end:
-            log.msg("Closing log, sending result of the command %s" % (self.cmd, ))
+            d.addCallback(lambda dummy: log.msg("Closing log, sending result of the command %s (%s)" % (self.cmd, dummy)))
             def _gotResults(results):
                 self.setStatus(self.cmd, results)
                 return results
-            d.addCallback(_gotResults)
             d.addCallback(lambda _: self.createSummary(self.cmd.logs['stdio']))
             d.addCallback(lambda _: self.evaluateCommand(self.cmd)) 
+            # Probably this is bad idea. Judging the result of whole step
+            # solely based on result of last command.
+            # Should fix this.
+            d.addCallback(_gotResults)
             d.addCallbacks(self.finished, self.checkDisconnect)
-            d.addErrback(self.failed)
-        return d
 
+        d.addErrback(self.failed)
+        return d
+        
     def _sourcedirIsUpdatable(self, _):
         cmd = LoggedRemoteCommand('stat', {'file': self.workdir + '/.hg'})
         cmd.useLog(self.stdio_log, False)
         d = self.runCommand(cmd)
         def _fail(tmp):
-            if cmd.rc == 1:
+            if cmd.rc != 0:
                 return False
             return True
         d.addCallback(_fail)
@@ -184,24 +193,43 @@ class Mercurial(Source):
         return d
 
     def incremental(self, _):
-        clobber = None
+        clobber = False
         d = defer.succeed(_)
         d.addCallback(self._getCurrentBranch)
         def _compare(current_branch):
             if current_branch != self.branch:
-                msg = "Working dir is on in-repo branch '%s' and build needs '%s'." % (current_branch, self.branch)
+                msg = "Working dir is on in-repo branch '%s' and build needs '%s'." % \
+                    (current_branch, self.branch)
                 if self.clobberOnBranchChange:
                     msg += ' Cloberring.'
                     clobber = True
                 else:
                     msg += ' Updating.'
-                    clobber = False
                 log.msg(msg)
 
         d.addCallback(_compare)
+
+        # Fix me
         if clobber:
             d.addCallback(self.doClobber)
         else:
             d.addCallback(self.doVCUpdate)
         
         return d
+
+    def clean(self, _):
+        command = ['--config', 'extensions.purge=', 'purge']
+        d = defer.succeed(0)
+        d.addCallback(lambda _: self._dovccmd(command, False))
+        # Check integrity of the repo
+        d.addCallback(lambda _: self._dovccmd(['pull', '--update', self.repourl], False))
+        return d
+
+    def fresh(self, _):
+        command = ['--config', 'extensions.purge=', 'purge', '--all']
+        d = defer.succeed(0)
+        d.addCallback(lambda _: self._dovccmd(command, False))
+        # Check integrity of the repo before and after pull/update
+        d.addCallback(lambda _: self._dovccmd(['pull', '--update', self.repourl], False))
+        return d
+    
