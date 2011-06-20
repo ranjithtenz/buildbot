679af38bab0e4d058508b88b755502315e2c1ab7
Pradeepkumar Gayam <in3xes@gmail.com>
2011-06-21 00:25:14 +0530

    Committing unfinished work to merge master into gsoc to fix #1992

--- master/buildbot/steps/source/git.py
+++ master/buildbot/steps/source/git.py
@@ -24,35 +24,56 @@ from buildslave.exceptions import AbandonChain
 class Git(Source):
     """ Class for Git with all the smarts """
     name='git'
+    renderables = [ "repourl"]
 
-    renderables = [ "repourl" ]
-
-    def __init__(self, repourl=None,
-                 branch='master',
-                 mode='incremental',
-                 method=None,
-                 ignore_ignores=None,
-                 **kwargs):
+    def __init__(self, repourl=None, branch='master', mode='incremental',
+                 method=None,ignore_ignores=None, submodule=False,
+                 progress=False, **kwargs):
         """
         @type  repourl: string
         @param repourl: the URL which points at the git repository
 
         @type  branch: string
-        @param branch: The default to check branch. Default value 'master'
+        @param branch: The branch or tag to check out by default. If
+                       a build specifies a different branch, it will
+                       be used instead of this.
 
+        @type  submodules: boolean
+        @param submodules: Whether or not to update (and initialize)
+                       git submodules.
 
+        @type  mode: string
+        @param mode: Type of checkout. Described in docs.
+
+        @type  method: string
+        @param method: Full builds can be done is different ways. This parameter
+                       specifies which method to use.
+
+        @type  progress: boolean
+        @param progress: Pass the --progress option when fetching. This
+                         can solve long fetches getting killed due to
+                         lack of output, but requires Git 1.7.2+.
         """
 
-        self.branch = branch
-        self.repourl = repourl
-        self.method = method
+        self.branch    = branch
+        self.method    = method
+        self.progress  = progress
+        self.repourl   = repourl
+        self.submodule = submodule
         Source.__init__(self, **kwargs)
-        self.addFactoryArguments(repourl=repourl,
+        self.addFactoryArguments(branch=branch,
                                  mode=mode,
                                  method=method,
+                                 progress=progress,
+                                 repourl=repourl,
+                                 submodule=submodule,
                                  )
+
         self.mode = mode
         self.repourl = self.repourl and _ComputeRepositoryURL(self.repourl)
+        assert self.mode in ['incremental', 'full']
+        if self.mode == 'full':
+            assert self.method in ['clean', 'fresh', 'clobber']
         
     def startVC(self, branch, revision, patch):
         
@@ -61,28 +82,47 @@ class Git(Source):
             raise BuildSlaveTooOldError("slave is too old, does not know "
                                         "about git")
         self.branch = branch or 'master'
+        self.revision = revision
         self.stdio_log = self.addLog("stdio")
 
         if self.mode == 'incremental':
-            log.msg("fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")
             d = self.incremental()
+        elif self.mode == 'full':
+            d = self.full()
+
         d.addCallback(self.parseGotRevision)
         d.addCallback(self.finish)
         d.addErrback(self.failed)
         return d
 
+    def clean(self):
+        return defer.succeed(FAILURE)
+
+    def clobber(self):
+        return FAILURE
+
     def incremental(self):
         d = self._sourcedirIsUpdatable()
-        def _cmd(updatable):
-            if updatable:
-                command = ['pull', self.repourl]
+        def fetch(res):
+            #if revision exits checkout to that revision
+            # else fetch and update
+            if res != 0:
+                return self._dovccmd(['reset', '--hard', self.revision])
             else:
-                command = ['clone', self.repourl, '.']
-            return command
+                return self._fetch()
+            return d
 
-        d.addCallback(_cmd)
-        d.addCallback(self._dovccmd)
-        d.addCallback(self._abandonOnFailure)
+        def cmd(updatable):
+            d = defer.succeed(None)
+            if updatable:
+                if self.revision:
+                    d.addCallback(self._dovccmd(['cat-file', '-e', self.revison]))
+                else:
+                    d.addCallback(lambda _: 1)
+                d.addCallback(fetch)
+            else:
+                d.addCallback(self._full())
+        d.addCallback(cmd)
         return d
 
     def finish(self, res):
@@ -96,6 +136,17 @@ class Git(Source):
         d.addCallbacks(self.finished, self.checkDisconnect)
         d.addErrback(self.failed)
         return d
+
+    def fresh(self):
+        return FAILURE
+
+    def full(self):
+        if self.method == 'clean':
+            return self.clean()
+        elif self.method == 'fresh':
+            return self.fresh()
+        elif self.method == 'clobber':
+            return self.clobber()
 
     def parseGotRevision(self, _):
         d = self._dovccmd(['rev-parse', 'HEAD'])
@@ -127,6 +178,34 @@ class Git(Source):
             return cmd.rc
         d.addCallback(lambda _: evaluateCommand(cmd))
         d.addErrback(self.failed)
+        return d
+
+    def _fetch(self):
+        command = ['fetch', '-t', self.repourl, self.branch]
+        # If the 'progress' option is set, tell git fetch to output
+        # progress information to the log. This can solve issues with
+        # long fetches killed due to lack of output, but only works
+        # with Git 1.7.2 or later.
+        if self.progress:
+            command.append('--progress')
+        d = self._dovccmd(command)
+        def checkout():
+            if self.revision:
+                rev = self.revision
+            else:
+                rev = 'FETCH_HEAD'
+            command = ['reset', '--hard', head]
+            return self._dovccmd(command)
+        d.addCallback(checkout)
+        return d
+
+    def _full(self):
+        command = ['clone', self.repourl, '.']
+        if self.progress:
+            command.append('--progress')
+        d = self._dovccmd(command)
+        if self.revision:
+            d.addCallback(self._dovccmd(['reset', '--hard', self.revision]))
         return d
 
     def _sourcedirIsUpdatable(self):
